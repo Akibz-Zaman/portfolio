f20dcec06ecbc734e10c19eff96acd56885ae9d6
Akibz-Zaman <akibzaman28@gmail.com>
2025-03-27 17:05:36 +0600

    porfolio file uploaded

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1); // Remove the '#'
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 80, // Adjust for header height  (Increased to 80px)
+                behavior: 'smooth'
+            });
+        }
+    });
+});
